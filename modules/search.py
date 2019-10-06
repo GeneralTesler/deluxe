2a88513a34cb5f0aea4747d4b63b9f0111c4ab06
2XXE <GeneralTesler@users.noreply.github.com>
2019-10-06 18:27:46 -0400

    removed exiftool

--- modules/search.py
+++ modules/search.py
@@ -1,12 +1,13 @@
 from googlesearch import search
 
-def extensionsearch(target,num):
-    extensions = ['docx','pptx','pdf','xlsx', 'doc', 'ppt', 'xls']
+
+def extensionsearch(target, num):
+    extensions = ["docx", "pptx", "pdf", "xlsx", "doc", "ppt", "xls"]
     res = []
     for ext in extensions:
-        query = 'filetype:%s site:%s' % (ext,target)
+        query = "filetype:%s site:%s" % (ext, target)
         for url in search(query, stop=num):
-            e = url.split('.')[-1]
-            #if e in extensions:	
+            e = url.split(".")[-1]
+            # if e in extensions:
             res.append(url)
     return res
